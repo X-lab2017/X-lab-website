2d27cdd6ceffc99bb5b53836f988cc7601edec92
逝不等琴生 <39022409+zhicheng-ning@users.noreply.github.com>
2022-07-24 20:00:00 +0800

    feat: add workflow file  (#1)
    * feat: test github-page deploy
    
    * feat: add workflow file
    
    * feat: change yarn to npm in workflow files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://github.com/zhicheng-ning/',
+  baseUrl: '/X-lab-website/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'zhicheng-ning', // Usually your GitHub org/user name.
+  projectName: 'X-lab-website', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
